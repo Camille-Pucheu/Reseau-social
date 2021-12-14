0e39a77f73668af9f71c92f74a70bede6c21a88a
Camille Pucheu <camille.pucheu@gmail.com>
2021-12-14 14:50:38 +0100

    Parametrage pour deploiement sur heroku

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static(__dirname + '/public/dist/my-app'))
 
 app.use('/test', function (req,res) {
-    res.sendFile(path.join(__dirname + '/public/dist/my-app/index.html'));
+    res.sendFile(path.join(__dirname, '/public/dist/my-app/index.html'));
     console.log(__dirname);
 })
 
